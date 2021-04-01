34fecba5620dd14a4f4001f012c80339d79c92f1
wszeborj <wszeborj@o2.pl>
2021-04-01 20:38:32 +0200

    chapter06

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -1,7 +1,58 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, escape
 from vsearch import search4letters
 
 app = Flask(__name__)
+
+
+def log_request(req: 'flask_request', res: str) -> None:
+    dbconfig = {
+        'host': '127.0.0.1',
+        'user': 'vsearch',
+        'password': '',
+        'database': 'vsearchlogDB',
+    }
+    import mysql.connector
+
+    conn = mysql.connector.connect(**dbconfig)
+    cursor = conn.cursor()
+    _SQL = """insert into log
+    (phrase,  letters, ip, browser_string, results)
+    values
+    (%s, %s, %s, %s, %s)"""
+
+    cursor.execute(_SQL, (req.form['phrase'],
+                          req.form['letters'],
+                          req.remote_addr,
+                          req.user_agent.browser,
+                          res,))
+    conn.commit()
+
+    cursor.close()
+    conn.close()
+
+
+@app.route('/viewlog')
+def view_the_log() -> 'html':
+    contents = []
+    with open('vsearch.log') as log:
+        data = log.readlines()
+
+        for line in data:
+            contents.append([])
+            listone = line.split('|')
+            for item in listone:
+                contents[-1].append(escape(item))
+    titles = ('Dane z formularza', 'Adres klienta', 'Agent użytkownika', 'Wyniki')
+    return render_template(
+        'viewlog.html',
+        the_title='Widok logu',
+        the_row='titles',
+        the_data=contents,
+    )
+
+
+
+    return escape(contents)
 
 
 @app.route('/search4', methods=['POST'])
@@ -10,6 +61,7 @@ def do_search() -> 'html':
     letters = request.form['letters']
     title = 'Oto Twoje wyniki:'
     results = str(search4letters(phrase, letters))
+    log_request(request, results)
     return render_template('results.html',
                            the_phrase=phrase,
                            the_letters=letters,
